027e000775ee2f173db40f63aaf83b0f4f0a34d7
circlesweeper <101175952+circlesweeper@users.noreply.github.com>
2022-04-18 13:34:59 -0500

    Update cell.js

--- cell.js
+++ cell.js
@@ -4,7 +4,9 @@ class Cell {
     this.y = y;
     this.size = size;
     this.revealed = false;
+    
     this.flagged = false;
+    this.numFlagged = 0;
     
     this.isMine = false;
     this.numMines = 0;
@@ -18,10 +20,10 @@ class Cell {
     
     let num = 0;
     
-    for (let i = -1; i < 2; i++) {
-      for (let j = -1; j < 2; j++) {
-        const col = (x + i) % cols;
-        const row = (y + j) % rows;
+    for (let i = -1; i <= 1; i++) {
+      for (let j = -1; j <= 1; j++) {
+        const col = (x + i);
+        const row = (y + j);
         if (col > -1 && col < cols && row > -1 && row < rows) {
           if (arr[col][row].isMine) num++;
         }
@@ -30,6 +32,29 @@ class Cell {
     if (this.isMine) num--;
     
     this.numMines = num;
+    return num;
+  }
+  
+  countFlagged(arr) {
+    const x = this.x;
+    const y = this.y;
+    const cols = arr.length;
+    const rows = arr[0].length;
+    
+    let num = 0;
+    
+    for (let i = -1; i <= 1; i++) {
+      for (let j = -1; j <= 1; j++) {
+        const col = (x + i);
+        const row = (y + j);
+        if (col > -1 && col < cols && row > -1 && row < rows) {
+          if (arr[col][row].flagged) num++;
+        }
+      }
+    }
+    if (this.flagged) num--;
+    
+    this.numFlagged = num;
     return num;
   }
   
@@ -48,9 +73,34 @@ class Cell {
         const col = (this.x + i);
         const row = (this.y + j);
         if (col > -1 && col < cols && row > -1 && row < rows) {
-          const ref = arr[col][row];
-          if (!ref.isMine && !ref.revealed) ref.reveal(arr);
+          const cell = arr[col][row];
+          if (!cell.isMine && !cell.revealed) cell.reveal(arr);
         }
+      }
+    }
+  }
+  
+  clicked(arr) {
+    if (this.containsPoint(mouseX, mouseY)) {
+      if (mouseButton == LEFT) {
+        switch (this.revealed) {
+          case false:
+            if (this.flagged) this.flagged = false;
+            else if (this.isMine) lose();
+            else this.reveal(arr);
+            break;
+          case true:
+            if (this.flagged) this.flagged = false; 
+            
+            this.countFlagged(arr);
+            if (this.numFlagged === this.numMines) {
+              this.revealNeighbors(arr);
+            }
+            break;
+        }
+      }
+      if (mouseButton == RIGHT) {
+        this.flagged = !this.flagged;
       }
     }
   }
@@ -60,6 +110,23 @@ class Cell {
     
     if (this.numMines == 0) {
       this.floodFill(arr);
+    }
+  }
+  
+  revealNeighbors(arr) {
+    for (let i = -1; i <= 1; i++) {
+      for (let j = -1; j <= 1; j++) {
+        const col = (this.x + i);
+        const row = (this.y + j);
+        if (col > -1 && col < cols && row > -1 && row < rows) {
+          const cell = arr[col][row];
+          if (!cell.flagged && !cell.revealed && cell.isMine) {
+            lose();
+          } else if (!cell.isMine) {
+            cell.reveal(arr);
+          }
+        }
+      }
     }
   }
   
